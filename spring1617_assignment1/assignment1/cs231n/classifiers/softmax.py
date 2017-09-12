53f42c1f59b87657155aa4f1f7b1e3faa74a32ac
gt <gaotongxiao@gmail.com>
2017-09-12 11:42:12 +0800

    hi

--- spring1617_assignment1/assignment1/cs231n/classifiers/softmax.py
+++ spring1617_assignment1/assignment1/cs231n/classifiers/softmax.py
@@ -30,7 +30,34 @@ def softmax_loss_naive(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  num_classes = W.shape[1]
+  num_train = X.shape[0]
+  loss = 0.0
+  for i in xrange(num_train):
+    scores = X[i].dot(W)
+    tempsum = 0.0
+    max_score = np.amax(scores)
+    for j in xrange(num_classes):
+      scores[j] = np.exp(scores[j] - max_score)
+      tempsum += scores[j]
+    loss += -np.log(scores[y[i]] / tempsum)
+    
+      '''
+      margin = scores[j] - correct_class_score + 1 # note delta = 1
+      if margin > 0:
+        loss += margin
+        dW[:, j] += X[i]
+        dW[:, y[i]] -= X[i]
+      '''
+
+  # Right now the loss is a sum over all training examples, but we want it
+  # to be an average instead so we divide by num_train.
+  loss /= num_train
+  dW /= num_train
+
+  # Add regularization to the loss.
+  loss += reg * np.sum(W * W)
+  dW += 2 * reg * W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
